6f0e3bf34bcfbb93e24433a7c89d4ca86b9bb02e
gzxfy <135656929+gzxfy@users.noreply.github.com>
2026-02-21 18:33:32 -0800

    Implemented a login system
    Created a account creator and account login

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Login.hpp"
+using namespace std;
+// #include "Manager.hpp"
+int main()
+{
+    Login login;
+    login.displayMenu();
+    
+    
+    return 0;
+}
+
